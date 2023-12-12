695c41eb3650fa971e6855a07b2340d1cd18b07c
yalda mohasseli <yaldamohasseliym@gmail.com>
2023-12-12 10:46:23 +0330

    Finished

--- lib/main.dart
+++ lib/main.dart
@@ -10,16 +10,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var textFieldBorder = OutlineInputBorder(
+        borderRadius: BorderRadius.circular(8),
+        borderSide: const BorderSide(color: Colors.transparent));
     return MaterialApp(
       title: 'Login Page',
+      debugShowCheckedModeBanner: false,
       home: const MyHomePage(),
       theme: ThemeData(
           inputDecorationTheme: InputDecorationTheme(
+              contentPadding: EdgeInsets.zero,
               fillColor: Colors.white,
-              enabledBorder:
-                  OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
-              focusedBorder:
-                  OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
+              enabledBorder: textFieldBorder,
+              focusedBorder: textFieldBorder,
               filled: true)),
     );
   }
@@ -43,32 +46,38 @@ class _MyHomePageState extends State<MyHomePage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Image.asset('asset/image/logo.png', width: 150),
+              const SizedBox(height: 80),
+              Image.asset('asset/image/logo.png', width: 110),
               const SizedBox(height: 50),
               const Text(
                 'Login',
                 style: TextStyle(
                     color: Colors.white,
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold),
+                    fontSize: 40,
+                    fontWeight: FontWeight.w800),
               ),
               const SizedBox(height: 25),
-               TextField(
+              TextField(
                 keyboardType: TextInputType.emailAddress,
+                textInputAction: TextInputAction.next,
                 decoration: InputDecoration(
                   hintText: 'example@email.com',
-                  prefixIcon: SvgPicture.asset('asset/image/email.svg', ),
+                  prefixIcon: _TextFieldIcon(
+                      icon: SvgPicture.asset('asset/image/email.svg')),
                 ),
               ),
               const SizedBox(height: 15),
-               TextField(
+              TextField(
                 obscureText: true,
                 obscuringCharacter: '*',
                 keyboardType: TextInputType.visiblePassword,
                 decoration: InputDecoration(
-                  prefixIcon: SvgPicture.asset('asset/image/lock.svg', ),
+                  prefixIcon: _TextFieldIcon(
+                      icon: SvgPicture.asset('asset/image/lock.svg')),
                 ),
               ),
+              const SizedBox(height: 25),
+              const _LoginButton(),
               const SizedBox(height: 25),
               const Text(
                 'Forgot Password?',
@@ -78,8 +87,148 @@ class _MyHomePageState extends State<MyHomePage> {
                     fontSize: 20,
                     fontWeight: FontWeight.w300),
               ),
+              const SizedBox(height: 25),
+              const _OrWidget(),
+              const SizedBox(height: 25),
+              const _SignUpButton()
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class _TextFieldIcon extends StatelessWidget {
+  const _TextFieldIcon({required this.icon});
+
+  final Widget icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 40,
+      width: 40,
+      padding: const EdgeInsets.all(11),
+      decoration: BoxDecoration(
+        color: const Color(0xffDCDADA),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: icon,
+    );
+  }
+}
+
+class _LoginButton extends StatelessWidget {
+  const _LoginButton();
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+          content: const Text(
+            'Log In',
+            style: TextStyle(color: Colors.black, fontSize: 18),
+          ),
+          backgroundColor: Colors.amber,
+        ));
+      },
+      child: Container(
+        height: 45,
+        width: MediaQuery.sizeOf(context).width,
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+            color: const Color(0xff04D2FF),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.black.withOpacity(.43),
+                  offset: const Offset(0, 4),
+                  blurRadius: 5)
+            ]),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Log in',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20),
+            ),
+            // SizedBox(width: 5),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 11.0, horizontal: 4),
+              child: SvgPicture.asset('asset/image/enter.svg'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _OrWidget extends StatelessWidget {
+  const _OrWidget();
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Container(height: 2, color: Colors.black),
+        ),
+        const SizedBox(width: 16),
+        const Text('or', style: TextStyle(fontSize: 22)),
+        const SizedBox(width: 16),
+        Expanded(
+          child: Container(height: 2, color: Colors.black),
+        ),
+      ],
+    );
+  }
+}
+
+class _SignUpButton extends StatelessWidget {
+  const _SignUpButton();
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Container(
+        height: 50,
+        width: MediaQuery.sizeOf(context).width,
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.black.withOpacity(.43),
+                  offset: const Offset(0, 4),
+                  blurRadius: 5)
+            ],
+            border: Border.all(color: const Color(0xff039DC1), width: 5),
+            borderRadius: BorderRadius.circular(8),
+            color: const Color(0xff026D8E)),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Sign Up',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20),
+            ),
+            // SizedBox(width: 5),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 11.0, horizontal: 4),
+              child: SvgPicture.asset('asset/image/enter.svg'),
+            )
+          ],
         ),
       ),
     );
